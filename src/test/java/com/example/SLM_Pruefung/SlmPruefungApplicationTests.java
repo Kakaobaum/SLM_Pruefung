c9094de229e5ba8184c7eac316d69379d204591c
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2023-06-27 23:53:16 +0200

    Initial commit

---
+++ src/test/java/com/example/SLM_Pruefung/SlmPruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLM_Pruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmPruefungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
