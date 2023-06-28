1ce575e9de2286ceb6faad33c70ca2109504447c
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2023-06-28 20:28:36 +0200

    add modify feature

--- src/main/java/com/example/SLM_Pruefung/SlmPruefungApplication.java
+++ src/main/java/com/example/SLM_Pruefung/SlmPruefungApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlmPruefungApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SlmPruefungApplication.class, args);
 	}
 
